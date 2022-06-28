a93cbf64443b7ad70b0abbfddf14571b6b7065b4
Praveenh1812 <praveenh.xworkz@gmail.com>
2022-06-28 08:38:13 +0530

    Committing Engineer files

---
+++ AutoMobileEngineers.java
@@ -0,0 +1,12 @@
+class AutoMobileEngineer extends Engineer{
+
+	public AutoMobileEngineer(){
+		System.out.println("AutoMobile Engineer object is created");
+	}
+	
+	public void solvesProblemInVehicles(){
+		System.out.println("AutoMobile Engineer solves Problem with machines ");
+	}
+
+
+}
